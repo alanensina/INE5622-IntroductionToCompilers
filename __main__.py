952733353875fbd70de7f9ab191817e10e5765d3
alan <alanvinicius.ce@gmail.com>
2021-04-18 16:18:12 -0300

    Finished

---
+++ __main__.py
@@ -0,0 +1,23 @@
+import argparse
+from os import path
+from antlr4 import FileStream, CommonTokenStream
+
+from build.tajaLexer import tajaLexer
+from build.tajaParser import tajaParser
+
+def main():
+    # Argument parser
+    parser_args = argparse.ArgumentParser(prog='Taja', description='Taja interpreter')
+    parser_args.add_argument('input', type=str, help='source code')
+    args = parser_args.parse_args()
+
+    input_stream = FileStream(args.input)
+
+    lexer = tajaLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = tajaParser(stream)
+
+    tree = parser.start()
+
+if __name__ == '__main__':
+    main()
